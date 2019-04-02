6171188bd48b0454382fe68e56e122961fbfcd1a
mukeunzi <eunji057@gmail.com>
2019-04-02 11:57:39 +0900

    solve baek1001

---
+++ algorithm/problem/src/problem/baek1001.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.*;
+
+public class baek1001 {
+
+	public static void main(String[] args) {
+		int a, b;
+		
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextInt();
+		b = scan.nextInt();
+		
+		System.out.println(a - b);
+	}
+
+}
