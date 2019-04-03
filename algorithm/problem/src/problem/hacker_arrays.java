eab674669bde3215e3a4e66a55a103612b9d188e
mukeunzi <eunji057@gmail.com>
2019-04-03 10:47:39 +0900

    solve hacker_arrays

--- algorithm/problem/src/problem/hacker_arrays.java
+++ algorithm/problem/src/problem/hacker_arrays.java
@@ -5,13 +5,19 @@ import java.util.*;
 public class hacker_arrays {
 
 	public static void main(String[] args) {
-		int a, b;
+		int arrLen;
 		
 		Scanner scan = new Scanner(System.in);
-		a = scan.nextInt();
-		b = scan.nextInt();
+		arrLen = scan.nextInt();
 		
-		System.out.println(a + b);
+		int arr[] = new int[arrLen];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		for(int i=arr.length-1; i>=0; i--) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 
 }
