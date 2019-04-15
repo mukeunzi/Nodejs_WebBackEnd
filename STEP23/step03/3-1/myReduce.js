0a908e05f33d480af799d7894bb655abccd4acb9
mukeunzi <eunji057@gmail.com>
2019-04-15 20:29:29 +0900

    forEach문 수정

--- STEP23/step03/3-1/myReduce.js
+++ STEP23/step03/3-1/myReduce.js
@@ -1,32 +1,22 @@
 // 문제. Array의 reduce()와 같이 동작하는 메서드 만들기
 
 const myReduce = (arr, callback, initialValue) => {
-    let acc;
+    let acc = 0;
 
+    // initialValue에 관계없이 arr의 요소를 다 더하니까 forEach를 하나만 사용해도 되구만
     if(typeof initialValue === "undefined"){
-        acc = arr[0];
-        // i가 1일때 callback 함수의 연산(+, *)을 통해 리턴 값을 acc에 누적하고
-        // for문의 길이만큼 반복한다
-        for(let i=1; i<arr.length; i++){
-            acc = callback(acc, arr[i]);
-        }
-    } else {
-        acc = initialValue;
-        for(let i=0; i<arr.length; i++){
-            acc = callback(acc, arr[i]);
-        }
+        initialValue = 0;
     }
+    acc = initialValue;
+    arr.forEach(element => { 
+        acc = callback(acc, element);
+    });
 
     return acc;
 }
 
-const resultSum = myReduce([1, 2, 3, 4, 5], (prev, value) => {
-    return prev + value;
-});
-
-const resultMul = myReduce([1, 2, 3, 4, 5], (prev, value) => {
-    return prev * value;
-});
+const resultSum = myReduce([1, 2, 3, 4, 5], (prev, value) => { return prev + value; });
+const resultMul = myReduce([1, 2, 3, 4, 5], (prev, value) => { return prev * value; }, 1);
 
 console.log(resultSum);
 console.log(resultMul);
