ba3c9e786d138b3f2593c48f63e4d05a47813492
mukeunzi <eunji057@gmail.com>
2019-04-20 22:41:18 +0900

    todo data satatus별로 분리한 todoList 생성

--- STEP23/step04/self/todo.js
+++ STEP23/step04/self/todo.js
@@ -1,3 +1,12 @@
-const data = module.require('./data');
+const todos = module.require('./data');
 
-console.log(data);
+const todoList = todos.reduce((acc, cur) => {
+	if (acc[cur.status] === undefined) {
+		acc[cur.status] = [cur.name];
+	} else {
+		acc[cur.status].push(cur.name);
+	}
+	return acc;
+}, {});
+
+console.log(todoList);
