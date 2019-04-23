15bedfe1bd87d396a5b881fe66c166a6432e568e
mukeunzi <eunji057@gmail.com>
2019-04-23 14:59:15 +0900

    step 5-2 스켈레톤코드

---
+++ STEP23/step05/5-2/todo.js
@@ -0,0 +1,21 @@
+const Todo = class {
+	// show
+	//매개변수 all, status(todo,done,doing)
+	// 매개변수에 해당하는 데이터 출력
+	// add
+	// 매개변수 name,tag
+	// id생성
+	// name과 id tag 로 만들어진 데이터 추가
+	// showall 1초뒤에 실행
+	// delete
+	// 매개변수 id
+	// id에 해당하는 데이터 삭제
+	// showall 1초뒤에 실행
+	// 존재하지 않는 id경우 출력
+	// update
+	// 매개변수 id, status
+	// id에 해당하는 데이터를 status로 상태 변경
+	// 3초 뒤에 name 이 status 로 변경되었ㅅ 출력
+	// 1초뒤 show all
+	// 존재하지 않는 id경우, 같은상태로 update경우 안내 출력
+};
