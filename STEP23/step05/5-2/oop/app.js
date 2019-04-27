c1e699c7d7e6f1a843310f66bace67ad0ce0f716
mukeunzi <eunji057@gmail.com>
2019-04-27 21:35:03 +0900

    feat : 예외처리 항목 추가
    - 예외처리 항목 추가

--- STEP23/step05/5-2/oop/app.js
+++ STEP23/step05/5-2/oop/app.js
@@ -1,4 +1,5 @@
-const todo = module.require('./todo.js');
+const Todo = module.require('./todo.js');
+const todoList = module.require('./data.js');
 const readline = require('readline');
 const errorMessage = module.require('./errorMessage.js');
 
@@ -6,34 +7,34 @@ const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-const myTodo = new todo(rl);
+const myTodo = new Todo(todoList, rl);
 
 rl.setPrompt('명령하세요 : ');
 
 rl.prompt();
-rl.on('line', input => {
-	const inputArray = input.split('$');
-	if (inputArray.length === 1) {
-		return myTodo.printError('COMMAND_ERROR');
-	}
-	const action = inputArray.splice(0, 1)[0];
-	const condition = inputArray;
 
-	excuteTodo(action, condition);
+rl.on('line', input => {
+	excuteTodo(input);
 });
 
-const excuteTodo = (action, condition) => {
-	const regExp = /^show$|^add$|^delete$|^update$/;
-	const matchRegExp = action.match(regExp);
-
+const excuteTodo = input => {
 	try {
+		const inputArray = input.split('$');
+		if (inputArray.length > 3) {
+			throw new Error('COMMAND_ERROR');
+		}
+		const action = inputArray.splice(0, 1)[0];
+		const condition = inputArray;
+		const regExp = /^show$|^add$|^delete$|^update$|^undo$|^redo$/;
+		const matchRegExp = action.match(regExp);
+
 		if (matchRegExp === null) {
-			throw new Error(errorMessage.COMMAND_ERROR);
+			throw new Error('COMMAND_ERROR');
 		} else {
 			myTodo[action](...condition);
 		}
 	} catch (e) {
-		console.log(e.message);
+		console.log(errorMessage[e.message]);
 		rl.prompt();
 	}
 };
