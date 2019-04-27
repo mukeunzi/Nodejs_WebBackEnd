242f2d2d59f046871539d3ba3fe9e969e0521e6c
mukeunzi <eunji057@gmail.com>
2019-04-27 21:27:44 +0900

    feat : create Log.js
    - 작업 로그를 남기기 위한 addLog 메소드  작성
    - 작업을 되돌릴 수 있는 undo 메소드 작성
    - 되돌린 작업을 현재상태로 되돌리는 redo 메소드 작성
    - undo/redo 명령을 처리하는 alterData 메소드 작성

---
+++ STEP23/step05/5-2/oop/Log.js
@@ -0,0 +1,78 @@
+const todoList = module.require('./data.js');
+module.exports = class Log {
+	constructor() {
+		(this.queue = []), (this.index = -1);
+	}
+
+	addLog(action, prevData, nextData, todoListIndex) {
+		if (this.queue.length > 4) {
+			this.queue.shift();
+		}
+
+		while (this.queue.length > this.index + 1) {
+			this.queue.pop();
+		}
+
+		this.queue[++this.index] = {
+			action: action,
+			prevData: prevData,
+			nextData: nextData,
+			todoListIndex: todoListIndex
+		};
+	}
+
+	undo() {
+		if (this.index < 0) {
+			throw new Error('UNDO_ERROR');
+		}
+
+		const action = this.queue[this.index].action;
+		const prevData = this.queue[this.index].prevData;
+		const nextData = this.queue[this.index].nextData;
+		const todoListIndex = this.queue[this.index].todoListIndex;
+
+		if (action === 'add') {
+			this.alterData(todoListIndex, 1);
+		} else if (action === 'delete') {
+			this.alterData(todoListIndex, 0, prevData);
+		} else if (action === 'update') {
+			this.alterData(todoListIndex, 1, prevData);
+		}
+		console.log(
+			`"${nextData.id}"번 항목 '${nextData.name}'이(가) ${nextData.status} 에서 ${prevData.status}로 변경되었습니다.`
+		);
+		this.index--;
+	}
+
+	redo() {
+		if (this.index >= this.queue.length - 1) {
+			throw new Error('REDO_ERROR');
+		}
+
+		this.index++;
+		const action = this.queue[this.index].action;
+		const prevData = this.queue[this.index].prevData;
+		const nextData = this.queue[this.index].nextData;
+		const todoListIndex = this.queue[this.index].todoListIndex;
+
+		if (action === 'add') {
+			this.alterData(todoListIndex, 0, nextData);
+		} else if (action === 'delete') {
+			this.alterData(todoListIndex, 1);
+		} else if (action === 'update') {
+			this.alterData(todoListIndex, 1, nextData);
+		}
+
+		console.log(
+			`"${nextData.id}"번 항목 '${nextData.name}'이(가) ${prevData.status} 에서 ${nextData.status}로 변경되었습니다.`
+		);
+	}
+
+	alterData(todoListIndex, deleteCount, data) {
+		if (data === undefined) {
+			todoList.splice(todoListIndex, deleteCount);
+		} else {
+			todoList.splice(todoListIndex, deleteCount, data);
+		}
+	}
+};
