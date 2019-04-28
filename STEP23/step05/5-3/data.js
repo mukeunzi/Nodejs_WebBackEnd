4b2a7b97c323d37e359745238963f1044a4c265e
mukeunzi <eunji057@gmail.com>
2019-04-29 01:09:53 +0900

    feat : add todo data
    - 데이터 추가

---
+++ STEP23/step05/5-3/data.js
@@ -0,0 +1,32 @@
+module.exports = [
+	{
+		name: '자바스크립트 공부하기',
+		tags: ['programming', 'javascript'],
+		status: 'todo',
+		id: 12123123
+	},
+	{
+		name: '그림 그리기',
+		tags: ['picture', 'favorite'],
+		status: 'doing',
+		id: 312323
+	},
+	{
+		name: '고양이 모래 주문하기',
+		tags: ['cat', 'shopping'],
+		status: 'todo',
+		id: 78956
+	},
+	{
+		name: '밤코하기',
+		tags: ['study', 'favorite'],
+		status: 'done',
+		id: 46588
+	},
+	{
+		name: '워홀가기',
+		tags: ['travel', 'favorite'],
+		status: 'todo',
+		id: 19846
+	}
+];
