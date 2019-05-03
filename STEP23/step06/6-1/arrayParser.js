ad3e8a753b8f401ed63e2e0508de35a9c0f2c54d
mukeunzi <eunji057@gmail.com>
2019-05-03 19:28:58 +0900

    feat : parser 메소드 생성
    - parser() : token의 값을 분석한 후 result 생성
    - arrayParser() : 클래스 실행 메서드

--- STEP23/step06/6-1/arrayParser.js
+++ STEP23/step06/6-1/arrayParser.js
@@ -8,9 +8,41 @@ class ArrayParser {
 
 		return strToken;
 	}
+
+	parser(strToken) {
+		const delSignToken = [];
+
+		if (strToken[0].includes('[') && strToken[strToken.length - 1].includes(']')) {
+			const signRegExp = /^\[|\]$|\,/g;
+
+			for (let str of strToken) {
+				delSignToken.push(str.replace(signRegExp, ''));
+			}
+
+			this.result.type = 'array';
+		}
+
+		for (let token of delSignToken) {
+			let childObj = {};
+			if (!isNaN(token)) {
+				token = Number(token);
+			}
+			childObj['type'] = typeof token;
+			childObj['value'] = token;
+			childObj['child'] = [];
+
+			this.result.child.push(childObj);
+		}
+	}
+
+	arrayParser(str) {
+		const token = this.lexer(str);
+		this.parser(token);
+		console.log(this.result);
+	}
 }
 
 const str = '[123, 22, 33]';
-// const myArrayParser = new ArrayParser();
-// const jsonStr = myArrayParser.arrayParser(str);
+const myArrayParser = new ArrayParser();
+const jsonStr = myArrayParser.arrayParser(str);
 // console.log(JSON.stringify(jsonStr, null, 2));
